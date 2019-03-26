1f631e36a71fc1c0d2753892281cdb8ced4244bf
ariefky <48465565+ariefky@users.noreply.github.com>
2019-03-26 12:10:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //Mengeset nilai properties objek saya dan kamu
+            saya.Nim = "131415";
+            saya.Nama = "Paijo";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "121314";
+            kamu.Nama = "Bemo";
+            kamu.Ipk = 4f;
+
+            //Memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Membuat objek dari class
+            TaxiOnline taxi = new TaxiOnline();
+
+            //Mengeset nilai properties
+            taxi.DriverName = "Bemo";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 1;
+
+            //Memanggil method
+            taxi.TaxiInfo();
+            taxi.Menjemput();
+            taxi.Mengantar();
+
+            Console.ReadKey();
+        }
+    }
+}
